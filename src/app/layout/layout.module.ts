cfcf03ae8d10af4fdfc30ff2b41eb0d103908817
Babak <babak.jahanbani@yahoo.com>
2022-07-01 00:24:59 +0430

    adding ordering to home and finalizing UI

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,10 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
-import { OrderingComponent } from './global/ordering/ordering.component';
 
 @NgModule({
-  declarations: [LayoutComponent, OrderingComponent],
+  declarations: [LayoutComponent],
   imports: [CommonModule, LayoutRoutingModule],
 })
 export class LayoutModule {}
--- src/app/layout/pages/home/home.module.ts
+++ src/app/layout/pages/home/home.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { OrderingComponent } from '../../global/ordering/ordering.component';
 
 @NgModule({
-  declarations: [HomeComponent, FooterComponent],
+  declarations: [HomeComponent, FooterComponent, OrderingComponent],
   imports: [CommonModule, HomeRoutingModule],
 })
 export class HomeModule {}
