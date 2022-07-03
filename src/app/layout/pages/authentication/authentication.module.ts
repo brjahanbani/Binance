a31a98edc5e95bf01a26651d5dd8d67d4d7b510f
Babak <babak.jahanbani@yahoo.com>
2022-07-03 13:33:49 +0430

    Authentication routing and its components

--- src/app/layout/pages/authentication/authentication.module.ts
+++ src/app/layout/pages/authentication/authentication.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { AuthenticationComponent } from './authentication.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { SignoutComponent } from './signout/signout.component';
 
 
 @NgModule({
   declarations: [
-    AuthenticationComponent
+    AuthenticationComponent,
+    SigninComponent,
+    SignupComponent,
+    SignoutComponent
   ],
   imports: [
     CommonModule,
