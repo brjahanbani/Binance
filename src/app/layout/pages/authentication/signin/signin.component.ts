d3bdc3f041b5152ecc024e98af91192bccafa0a5
Babak <babak.jahanbani@yahoo.com>
2022-07-03 15:20:56 +0430

    sign in & sign up

--- src/app/layout/pages/authentication/signin/signin.component.ts
+++ src/app/layout/pages/authentication/signin/signin.component.ts
@@ -1,15 +1,65 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/layout/services/auth.service';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.css']
+  styleUrls: ['./signin.component.css'],
 })
 export class SigninComponent implements OnInit {
+  constructor(private authService: AuthService, private router: Router) {}
+  form = new FormGroup<any>({
+    username: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+    ]),
+    password: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(20),
+    ]),
+  });
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onSubmit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    console.log(this.form.value);
+    this.authService.signin(this.form.value).subscribe(
+      (response) => {
+        this.router.navigate(['/trades']);
+      },
+      (error: any) => {
+        if (!error.status) {
+          this.form.setErrors({ noConnection: true });
+          return;
+        }
+        if (error.error.username || error.error.password) {
+          this.form.setErrors({ credential: true });
+        } else {
+          this.form.setErrors({ unknownError: true });
+          return;
+        }
+      }
+    );
   }
 
+  get f() {
+    return this.form.controls;
+  }
+
+  ngOnInit(): void {}
+
+  showErrorPasswordDontMatch() {
+    return (
+      this.form.controls['password'].dirty &&
+      this.form.controls['password'].touched &&
+      this.form.controls['username'].dirty &&
+      this.form.controls['username'].touched
+    );
+  }
 }
--- src/app/layout/pages/authentication/signup/signup.component.ts
+++ src/app/layout/pages/authentication/signup/signup.component.ts
@@ -1,15 +1,80 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/layout/services/auth.service';
+import { MatchPasswordsService } from 'src/app/layout/validators/match-passwords.service';
+import { UniqueUsernameService } from 'src/app/layout/validators/unique-username.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  form = new FormGroup<any>(
+    {
+      username: new FormControl<string>(
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(20),
+        ],
+        [this.uniqueUsername.validate.bind(this.uniqueUsername)]
+      ),
+      password: new FormControl<string>('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(20),
+      ]),
+      passwordConfirmation: new FormControl<string>('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(20),
+      ]),
+    },
+    {
+      validators: [this.matchPasswords.validate],
+    }
+  );
+  constructor(
+    private matchPasswords: MatchPasswordsService,
+    private uniqueUsername: UniqueUsernameService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get f() {
+    return this.form.controls;
   }
 
+  onSubmit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    this.authService.signup(this.form.value).subscribe(
+      (response) => {
+        this.router.navigate(['/trades']);
+      },
+      (error) => {
+        if (!error.status) {
+          this.form.setErrors({ noConnection: true });
+        } else {
+          this.form.setErrors({ unknownError: true });
+        }
+      }
+    );
+  }
+
+  showErrorPasswordDontMatch() {
+    return (
+      this.form.controls['password'].dirty &&
+      this.form.controls['password'].touched &&
+      this.form.controls['passwordConfirmation'].dirty &&
+      this.form.controls['passwordConfirmation'].touched
+    );
+  }
+
+  ngOnInit(): void {}
 }
