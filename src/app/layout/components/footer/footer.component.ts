cc8f05ba67f37440720fb0391f885b28f1859f79
Babak <babak.jahanbani@yahoo.com>
2022-07-03 15:46:08 +0430

    final footer

--- src/app/layout/components/footer/footer.component.ts
+++ src/app/layout/components/footer/footer.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
+  constructor(public authService: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
