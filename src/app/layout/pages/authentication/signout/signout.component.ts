9fe93f1fec68fe73fa6998bc35f992533ddd3213
Babak <babak.jahanbani@yahoo.com>
2022-07-03 15:46:30 +0430

    signout

--- src/app/layout/pages/authentication/signout/signout.component.ts
+++ src/app/layout/pages/authentication/signout/signout.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { delay } from 'rxjs';
+import { AuthService } from 'src/app/layout/services/auth.service';
 
 @Component({
   selector: 'app-signout',
   templateUrl: './signout.component.html',
-  styleUrls: ['./signout.component.css']
+  styleUrls: ['./signout.component.css'],
 })
 export class SignoutComponent implements OnInit {
-
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
+    this.authService
+      .signout()
+      // .pipe(delay(0))
+      .subscribe(() => {
+        //TODO: redirect to home
+        this.router.navigate(['/']);
+      });
   }
-
 }
